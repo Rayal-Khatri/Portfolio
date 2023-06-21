ad2fb299ba048a073e87d0bffa3bb9f31f5cfd74
Rayal kc <kcrayalgw@gmail.com>
2023-06-21 23:05:50 +0545

    Added the Sliders

--- Portfolio1/Js/Script.js
+++ Portfolio1/Js/Script.js
@@ -1,109 +1,103 @@
 const navMenu = document.getElementById('nav_menu'),
-        navToggle = document.getElementById('nav_toggle'),
-        navClose = document.getElementById('nav_close')
-if(navToggle)
-{
-    navToggle.addEventListener('click', () =>{
+    navToggle = document.getElementById('nav_toggle'),
+    navClose = document.getElementById('nav_close')
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         console.log(navMenu);
         navMenu.classList.add('show_menu')
     })
 }
 
-if(navClose)
-{
-    navClose.addEventListener('click',() =>
-    {
+if (navClose) {
+    navClose.addEventListener('click', () => {
         linkAction();
     })
 }
 
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll('.nav_link')
-function linkAction()
-{
+function linkAction() {
     navMenu.classList.remove('show_menu')
-}        
+}
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 const skillsContent = document.getElementsByClassName('skills_content')
-    skillsHeader = document.querySelectorAll('.skills_header')
+skillsHeader = document.querySelectorAll('.skills_header')
 
-function toggleSkills()
-{
+function toggleSkills() {
     let itemClass = this.parentNode.className
-    for(i=0; i<skillsContent.length; i++)
-    {    
-        skillsContent[i].className ='skills_content skills_close'
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills_close'
     }
-    if(itemClass==='skills_content skills_close')
-    {
+    if (itemClass === 'skills_content skills_close') {
         this.parentNode.className = 'skills_content skills_open'
     }
 }
-skillsHeader.forEach((el)=>
-{
-    el.addEventListener('click',toggleSkills)
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
 })
 
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab => 
-{
-    tab.addEventListener('click',()=>
-    {
-        const target = document.querySelector(tab.dataset.target)
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.target)
     
-        tabContents.forEach(tabContent =>
-        {
-            tabContent.classList.remove('qualification_active')
+            tabContents.forEach(tabContent => {
+                console.log("Remove active is triggered");
+                tabContent.classList.remove('qualification_active')
+            })
+            target.classList.add('qualification_active')
+    
+            tabs.forEach(otherTab => {
+                otherTab.classList.remove('qualification_active')
+            })
+            tab.classList.add('qualification_active')
         })
-        target.classList.add('qualification_active')
-
-        tab.forEach(tab =>
-        {
-            tab.classList.remove('qualification_active')
-        })
-        tab.classList.add('qualification_active')
     })
-})
+    
 
 // Services Portion
 const modalViews = document.querySelectorAll('.services_modal'),
-    modalBtns= document.querySelectorAll('.services_button'),
+    modalBtns = document.querySelectorAll('.services_button'),
     modalCloses = document.querySelectorAll('.services_modal_close')
-let modal = function (modalClick)
-{
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active_modal')
 }
 
-modalBtns.forEach((modalBtn, i) => 
-{
-    modalBtn.addEventListener('click', ()=>
-    {
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
         modal(i)
     })
 })
-modalCloses.forEach((modalClose) =>
-{  
-    modalClose.addEventListener('click', ()=>
-    {
-        modalViews.forEach((modalView) =>
-        {
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active_modal')
         })
     })
 })
-
-let swiper = new Swiper('.portfolio_container', {
-    cssMode: true,
-    navigation: {
+ 
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Swiper
+    const swiper = new Swiper('.swiper', {
+      // Configure Swiper options here
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true, 
+      navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true,
-});
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  });
+  
